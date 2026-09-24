1d47728da150bf3e31140bb4cc718fbd3b261778
AlexandrC <acapovsky@gmail.com>
2026-09-24 22:05:32 +0200

    build(bootstrap): wire modules and configure PostgreSQL

--- bootstrap/build.gradle.kts
+++ bootstrap/build.gradle.kts
@@ -7,11 +7,21 @@ plugins {
 dependencies {
     implementation(platform(libs.spring.boot.bom))
     implementation(project(":core"))
+    implementation(project(":app"))
+    implementation(project(":entrypoints"))
+    implementation(project(":output-adapters"))
 
     implementation(libs.spring.boot.starter.webmvc)
+    implementation(libs.spring.boot.starter.flyway)
     implementation(libs.kotlin.reflect)
     implementation(libs.jackson.module.kotlin)
+    runtimeOnly(libs.postgresql)
+    runtimeOnly(libs.flyway.database.postgresql)
 
     testImplementation(libs.spring.boot.starter.test)
+    testImplementation(libs.spring.boot.testcontainers)
+    testImplementation(libs.testcontainers.postgres)
+    testImplementation(libs.spring.boot.starter.webmvc.test)
     testRuntimeOnly(libs.junit.platform.launcher)
+
 }
